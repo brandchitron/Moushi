c9160b5d330f50c359207d4b507757f256a5a194
CHITRON BHATTACHARJEE <162599803+brandchitron@users.noreply.github.com>
2026-05-14 14:35:08 +0600

    Update PhotosScreen.jsx

--- src/components/screens/PhotosScreen.jsx
+++ src/components/screens/PhotosScreen.jsx
@@ -13,6 +13,9 @@ const photos = [
   "/images/2.jpg",
   "/images/3.jpg",
   "/images/4.jpg",
+  "/images/5.jpg",
+  "/images/6.jpg",
+  "/images/7.jpg",
 ]
 
 export default function PhotosScreen({ onNext }) {
